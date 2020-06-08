7d2f94c9f4af8653dbb859d2a7ab0852df119337
Vladimir Kononenko <vsolenaa@gmail.com>
2020-06-09 07:54:51 +0800

    redone architecture

--- main.py
+++ main.py
@@ -8,52 +8,63 @@ from src.helpers.dictionaryGetValueKeySeparated import dictionaryGetValueKeySepa
 
 while True:
     case = input('Select action: ' + dictionaryGetValueKeySeparated(ALGORITHM_choice) + '; e - EXIT: ').lower()
-    if case == ALGORITHM_choice['RSA']:
-        __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
-        deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
-
-    elif case == ALGORITHM_choice['LRR']:
-        __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
-        deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
-
-    elif case == ALGORITHM_choice['ECDSA']:
-        __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
-        deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
-
-    if case == ALGORITHM_choice['EL_GAMAL']:
-        __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
-        deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
-
-    if case == ALGORITHM_choice['MD5']:
-        __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
-        deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
-
-    if case == ALGORITHM_choice['DH']:
-        __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
-        deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
-
-    if case == ALGORITHM_choice['ECDH']:
-        __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
-        deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
-
-    if case == ALGORITHM_choice['EC_EL_GAMAL']:
-        __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
-        deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
-
-    if case == ALGORITHM_choice['AES']:
-        __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
-        deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
-
-    if case == ALGORITHM_choice['DES']:
-        __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
-        deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
-
-    if case == ALGORITHM_choice['SHA256']:
-        __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
-        deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
-
-    elif case == EXIT:
+    for alg in ALGORITHM_choice.keys():
+        if case == ALGORITHM_choice[alg]:
+            __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
+            deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
+    
+    if case == EXIT:
         end('Execution completed.')
 
-    else:
+    elif case not in ALGORITHM_choice.values():
         warning('Select algorithm.')
+
+    # if case == ALGORITHM_choice['RSA']:
+    #     __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
+    #     deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
+
+    # elif case == ALGORITHM_choice['LRR']:
+    #     __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
+    #     deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
+
+    # elif case == ALGORITHM_choice['ECDSA']:
+    #     __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
+    #     deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
+
+    # elif case == ALGORITHM_choice['EL_GAMAL']:
+    #     __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
+    #     deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
+
+    # elif case == ALGORITHM_choice['MD5']:
+    #     __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
+    #     deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
+
+    # elif case == ALGORITHM_choice['DH']:
+    #     __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
+    #     deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
+
+    # elif case == ALGORITHM_choice['ECDH']:
+    #     __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
+    #     deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
+
+    # elif case == ALGORITHM_choice['EC_EL_GAMAL']:
+    #     __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
+    #     deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
+
+    # elif case == ALGORITHM_choice['AES']:
+    #     __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
+    #     deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
+
+    # elif case == ALGORITHM_choice['DES']:
+    #     __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
+    #     deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
+
+    # elif case == ALGORITHM_choice['SHA256']:
+    #     __import__(ALGORITHM_MENU_MODULE_SPACE + dictionaryKeyParser(ALGORITHM_choice, case))
+    #     deleteModuleIfReturn(dictionaryKeyParser(ALGORITHM_choice, case))
+
+    # elif case == EXIT:
+    #     end('Execution completed.')
+
+    # else:
+    #     warning('Select algorithm.')
