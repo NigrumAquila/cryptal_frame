3ce28dce0c1531ab2b5900556ddec63e574f7c96
Vladimir Kononenko <vsolenaa@gmail.com>
2020-06-19 17:35:29 +0800

    cleaning

--- examples/fileHelpers/pickValidationFile.py
+++
@@ -1,5 +0,0 @@
-def pickValidationFile():
-    srcFilePath = pickFile(); filename, file_extension = splitext(srcFilePath)
-    if not isfile(filename + '.sign'): raise Exception('Signature not exist')
-    if file_extension == '.sign': raise Exception('You picked signature. Nonsense.')
-    return open(srcFilePath, 'rb'), open(filename + '.sign', 'rb')
